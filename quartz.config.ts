b7034651d850bd5a847cd334ef2f55641a7af78d
Michael Wilson <mw@michaelwilson.online>
2024-08-10 14:26:45 -0700

    Quartz sync: Aug 10, 2024, 2:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🗒️ My Notes",
+    pageTitle: "🤔 Michael's Thoughts",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
