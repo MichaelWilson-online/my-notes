7a6254839cc71e14011a751183175b46ad188bdd
Michael Wilson <mw@michaelwilson.online>
2024-08-10 14:10:02 -0700

    Quartz sync: Aug 10, 2024, 2:10 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "michaelwilson.online",
+    baseUrl: "blog.michaelwilson.online",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
