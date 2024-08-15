87228df25ed70e7994fd1937a939999511da2fed
Michael Wilson <mw@michaelwilson.online>
2024-08-15 12:21:30 -0700

    Quartz sync: Aug 15, 2024, 12:21 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,9 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      "GitHub ∙":"https://github.com/MichaelWilson-online",
-      "  Twitter ∙":"https://x.com/antiduplicity",
-      "  Tech Pastor Solutions":"https://techpastorsolutions.com",
+      "GitHub":"https://github.com/MichaelWilson-online",
+      " ∙ Twitter":"https://x.com/antiduplicity",
+      " ∙ Tech Pastor Solutions":"https://techpastorsolutions.com",
     },
   }),
 }
