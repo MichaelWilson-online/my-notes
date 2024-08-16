1ff514e3c98d75a04c9b182b531210a6b7d729d5
Michael Wilson <mw@michaelwilson.online>
2024-08-16 13:12:48 -0700

    Quartz sync: Aug 16, 2024, 1:12 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,7 +45,9 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+/*
+ *   Component.DesktopOnly(Component.Explorer()),
+*/
   ],
   right: [],
 }
